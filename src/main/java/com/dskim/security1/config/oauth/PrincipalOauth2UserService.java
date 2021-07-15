1bee8581a69e1bdbb2061000a207ccffdc354f2b
DongSubKim <dskim9218@gmail.com>
2021-07-15 20:32:57 +0900

    facebook, naver oauth2.0 login

--- src/main/java/com/dskim/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/dskim/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,5 +1,7 @@
 package com.dskim.security1.config.oauth;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -9,6 +11,10 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.dskim.security1.config.auth.PrincipalDetails;
+import com.dskim.security1.config.oauth.provider.FacebookUserInfo;
+import com.dskim.security1.config.oauth.provider.GoogleUserInfo;
+import com.dskim.security1.config.oauth.provider.NaverUserInfo;
+import com.dskim.security1.config.oauth.provider.OAuth2UserInfo;
 import com.dskim.security1.model.User;
 import com.dskim.security1.repository.UserRepository;
 
@@ -24,6 +30,8 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 	// google로부터 받은 userRequest 데이터에 대한 후처리 되는 함수
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		System.out.println("getClientRegistration: " + userRequest.getClientRegistration());
+		System.out.println("getAccessToken: " + userRequest.getAccessToken());
 		// userRequest.getClientRegistration() > registrationId로 어떤 OAuth로 로그인 했는 지 확인
 		// 가능.
 
@@ -31,21 +39,34 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		System.out.println("getAttributes: " + oauth2User.getAttributes());
 
 		// 회원가입을 강제로 진행해볼 예정
-		String provider = userRequest.getClientRegistration().getClientId(); // google
-		String providerId = oauth2User.getAttribute("sub");
-		String username = provider + "_" + providerId; // google_101046933161514265813
+		OAuth2UserInfo oAuth2UserInfo = null;
+		if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("Google login request");
+			oAuth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes());
+		} else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+			System.out.println("Facebook login request");
+			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
+		} else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+			System.out.println("Naver login request");
+			oAuth2UserInfo = new NaverUserInfo((Map) oauth2User.getAttributes().get("response"));
+		} else {
+			System.out.println("우리는 구글과 페이스북, 네이버 만 지원해요.");
+		}
+		String provider = oAuth2UserInfo.getProvider();
+		String providerId = oAuth2UserInfo.getProviderId();
+		String username = provider + "_" + providerId;
 		String password = bCryptPasswordEncoder.encode(username);
-		String email = oauth2User.getAttribute("email");
+		String email = oAuth2UserInfo.getEmail();
 		String role = "ROLE_USER";
 
 		User userEntity = userRepository.findByUsername(username);
 		if (userEntity == null) {
-			System.out.println("구글 로그인이 최초입니다.");
+			System.out.println("최초 로그인 요청입니다. ");
 			userEntity = User.builder().username(username).password(password).email(email).role(role).provider(provider)
 					.providerId(providerId).build();
 			userRepository.save(userEntity);
 		} else {
-			System.out.println("구글 로그인을 이미 한적이 있습니다. 당신은 자동회원가입이 되어 있습니다.");
+			System.out.println("이미 한적이 있습니다. 당신은 자동회원가입이 되어 있습니다.");
 		}
 
 		return new PrincipalDetails(userEntity, oauth2User.getAttributes());
